abcae863664f7c842aaf55ca4a72a9c11a65d137
Waseem Hassan <waseem.hassan119@gmail.com>
2026-07-27 15:11:54 +0200

    Cache recipe library application files

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,5 @@
-const CACHE = 'meal-planner-v3';
-const APP_SHELL = ['./', './index.html', './styles.css', './planner-engine.js', './app.js', './manifest.webmanifest', './icon.svg'];
-
+const CACHE = 'meal-planner-v5';
+const APP_SHELL = ['./', './index.html', './styles.css', './recipe-styles.css', './planner-engine.js', './data/recipe-profiles-1.js', './data/recipe-profiles-2.js', './data/recipes.js', './app/app-core.js', './app/app-views-primary.js', './app/app-views-lists.js', './app/app-views-recipes.js', './app/app-domain.js', './app/app-main.js', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)));
   self.skipWaiting();
